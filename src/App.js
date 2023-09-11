4c0d7ccd423af9b6fcfa59c86104b3e4361f77ab
yunji0387 <yunji0387@gmail.com>
2023-09-11 15:50:13 -0500

    test mui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-        Hello world!!!
+        {/* <Header /> */}
+        <Navbar />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header>
+        <h2>This is the header</h2>
+    </header>
+  );
+}
+
+export default Header;
